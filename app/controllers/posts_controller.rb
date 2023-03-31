f7e458888f3a231325079ace7f13a33ecfefff54
brickwall <sk292207@gmail.com>
2023-03-31 18:03:46 +0530

    Add show post feature and flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,21 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def create
+		@post = Post.new(allow_params)
+		if @post.save
+        	flash[:success] = "You successfully created the post"
+        	redirect_to post_path(@post.id) # go to show page for @post
+      	else
+        # code to set up error message
+        	render :new, status: :unprocessable_entity
+      	end
+	end
+
+	private
+
+	def allow_params
+		params.require(:post).permit(:title, :body)
+	end
 end
